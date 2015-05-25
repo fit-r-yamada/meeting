4dea79019438a8e77b6beb39d745b54f16e53800
fit-r-yamada <r_yamada@for-it.co.jp>
2015-05-25 19:38:54 +0900

    syuusei 1 but error
    error

--- sample03.py
+++ sample03.py
@@ -8,7 +8,15 @@ def fib(num):
         return fib(num - 2) + fib(num - 1)
 ret = 0
 list = []
-"""ret = fib(40)"""
+"""ret = fib(40)
 for i in range(0,30):
     list.append(fib(i))
-print(list)
+print(list)"""
+list = [0,1]
+def fibArray(list , num):
+    if num >= 2:
+        list[num] = list[num-1] + list[num-2]
+
+    return list
+ret = fibArray(list , 2)
+print(ret)
